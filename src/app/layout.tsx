cc7dd780ae6b18a67a33cbd62af62c0c69ad7f6a
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-11-30 16:19:16 +0100

    fix: moved hydrationwarning from html to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,11 +47,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FDFAE0]`}
-        suppressHydrationWarning
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FDFAE0] dark:bg-black`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <ThemeProvider
           attribute="class"
