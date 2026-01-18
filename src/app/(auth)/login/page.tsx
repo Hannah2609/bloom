0b54100889100a73040ecd751bf56e2aa0731210
Hannah2609 <hann7419@stud.kea.dk>
2026-01-18 14:16:18 +0100

    format

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -27,24 +27,26 @@ function LoginToastHandler() {
 
 function Page() {
   return (
-    <Suspense fallback={
-      <section className="grid min-h-svh px-8 lg:grid-cols-2 lg:p-3">
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-[350px]">
-            <LoginForm />
+    <Suspense
+      fallback={
+        <section className="grid min-h-svh px-8 lg:grid-cols-2 lg:p-3">
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-[350px]">
+              <LoginForm />
+            </div>
           </div>
-        </div>
-        <div className="relative hidden lg:block">
-          <Image
-            width={800}
-            height={600}
-            src="/placeholder.webp"
-            alt="Image"
-            className="absolute inset-0 h-full w-full rounded-4xl object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
-      </section>
-    }>
+          <div className="relative hidden lg:block">
+            <Image
+              width={800}
+              height={600}
+              src="/placeholder.webp"
+              alt="Image"
+              className="absolute inset-0 h-full w-full rounded-4xl object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </section>
+      }
+    >
       <section className="grid min-h-svh px-8 lg:grid-cols-2 lg:p-3">
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-[350px]">
